173c67fef253e3537239c2b3101f6e612cdfa628
Ekrem ASLAN <130939322+aslnekrem@users.noreply.github.com>
2023-09-15 21:25:25 +0200

    C01_relativeLocators.java

--- src/test/java/techproed/day06_Maven/C01_relativeLocators.java
+++ src/test/java/techproed/day06_Maven/C01_relativeLocators.java
@@ -15,6 +15,7 @@ public class C01_relativeLocators {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         //amazona gidelim
         driver.get("https://amazon.com");
+        
 
         //city bike aratalım
         WebElement aramaKutusu = driver.findElement(By.xpath("//*[@type='text']"));
